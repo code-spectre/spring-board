3232acf293347453469fe69c4dd192991aeaadd7
Eun Deok, Lee <pleed0215@gmail.com>
2023-05-15 07:33:42 +0900

    #5-db-testing: Auto stash before revert of "#5-db-testing"

--- springboard/src/main/java/com/example/springboard/config/JpaConfig.java
+++ springboard/src/main/java/com/example/springboard/config/JpaConfig.java
@@ -1,2 +1,18 @@
-package com.example.springboard.config;public class JpaConfig {
+package com.example.springboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO: spring security로 인증기능을 추가할 때에는 코드를 바꿔야 한다.
+        return () -> Optional.of("unnamed");
+    }
 }
