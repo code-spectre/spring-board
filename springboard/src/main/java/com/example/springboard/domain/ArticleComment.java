3318699fdd04853b3d22e9c95070ca0d709beed3
Eun Deok, Lee <pleed0215@gmail.com>
2023-05-14 14:47:05 +0900

    #4 - domain
    Article, ArticleComment added.

---
+++ springboard/src/main/java/com/example/springboard/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package com.example.springboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
