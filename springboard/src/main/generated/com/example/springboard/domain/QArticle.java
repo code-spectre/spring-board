dfc40d9c9f1a31c4ca2a5bffd1e383fde9b19867
Eun Deok, Lee <pleed0215@hotmail.com>
2023-05-20 14:04:09 +0900

    #3 API 구현 중
    Querydsl 설정 추가.

---
+++ springboard/src/main/generated/com/example/springboard/domain/QArticle.java
@@ -0,0 +1,61 @@
+package com.example.springboard.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QArticle is a Querydsl query type for Article
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QArticle extends EntityPathBase<Article> {
+
+    private static final long serialVersionUID = -1725195822L;
+
+    public static final QArticle article = new QArticle("article");
+
+    public final QAuditingFields _super = new QAuditingFields(this);
+
+    public final SetPath<ArticleComment, QArticleComment> articleComments = this.<ArticleComment, QArticleComment>createSet("articleComments", ArticleComment.class, QArticleComment.class, PathInits.DIRECT2);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final StringPath hashtag = createString("hashtag");
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final StringPath title = createString("title");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    //inherited
+    public final StringPath updatedBy = _super.updatedBy;
+
+    public QArticle(String variable) {
+        super(Article.class, forVariable(variable));
+    }
+
+    public QArticle(Path<? extends Article> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QArticle(PathMetadata metadata) {
+        super(Article.class, metadata);
+    }
+
+}
+
